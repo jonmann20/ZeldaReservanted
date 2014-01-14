e9584d385169e4b6210ab8355484ee017831666b
Jon Wiedmann <jonwiedmann@gmail.com>
2014-01-14 10:53:31 -0500

    anims

---
+++ Assets/Scripts/HeartSelectorMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeartSelectorMovement : MonoBehaviour {
+
+	float y = 0.7f;
+
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.W)){
+			transform.Translate(new Vector3(0, y, 0));
+		}
+
+
+		if(Input.GetKeyDown(KeyCode.S)){
+			transform.Translate(new Vector3(0, -y, 0));
+		}
+	}
+}
---
+++ Assets/SimpleSpriteLoopAnimation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleSpriteLoopAnimation : MonoBehaviour {
+
+	public Sprite[] spr;
+	public float fps;
+	
+	SpriteRenderer sprRend;
+	
+	void Start () {
+		sprRend = renderer as SpriteRenderer;
+	}
+	
+	void Update () {
+		int idx = (int)(Time.timeSinceLevelLoad * fps);
+		idx = idx % spr.Length;
+		sprRend.sprite = spr[idx];
+	}
+}
