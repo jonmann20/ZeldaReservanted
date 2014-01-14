cc2cbbadc783b209cab0c0ecca35da97a231a9ea
Jon Wiedmann <jonwiedmann@gmail.com>
2014-01-13 23:32:15 -0500

    3 scenes

---
+++ Assets/Link.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Link : MonoBehaviour {
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+		if(Input.GetKey(KeyCode.W)){
+
+		}	
+
+		if(Input.GetKey(KeyCode.S)){
+
+		}
+
+		if(Input.GetKey(KeyCode.A)){
+
+		}
+
+		if(Input.GetKey(KeyCode.D)){
+
+		}
+	}
+}
---
+++ Assets/TextCta.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextCta : MonoBehaviour {
+	public KeyCode key = KeyCode.Return;
+	public string nextScene;
+
+
+	void Update () {
+		if(Input.GetKey(key)){
+			Application.LoadLevel(nextScene);
+		}
+	}
+}
