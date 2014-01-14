ff4a807b79c3a2ae2122defe645367d5b1bf81d2
Jon Wiedmann <jonwiedmann@gmail.com>
2014-01-14 11:13:56 -0500

    link

--- Assets/Scripts/Link.cs
+++ Assets/Scripts/Link.cs
@@ -2,26 +2,36 @@
 using System.Collections;
 
 public class Link : MonoBehaviour {
-	
+
+	public Sprite[] spr;
+
+	float speed = 0.1f;
+
+	SpriteRenderer sprRend;
+
 	void Start () {
-	
+		sprRend = renderer as SpriteRenderer;
 	}
 
 	void Update () {
 		if(Input.GetKey(KeyCode.W)){
-
+			sprRend.sprite = spr[2];
+			transform.Translate(new Vector2(0, speed));
 		}	
 
 		if(Input.GetKey(KeyCode.S)){
-
+			sprRend.sprite = spr[0];
+			transform.Translate(new Vector2(0, -speed));
 		}
 
 		if(Input.GetKey(KeyCode.A)){
-
+			sprRend.sprite = spr[1];
+			transform.Translate(new Vector2(-speed, 0));
 		}
 
 		if(Input.GetKey(KeyCode.D)){
-
+			sprRend.sprite = spr[3];
+			transform.Translate(new Vector2(speed, 0));
 		}
 	}
 }
