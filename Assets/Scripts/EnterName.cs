70a0b52e3adb84ee761319fd292930a0000963d0
Jon Wiedmann <jonwiedmann@gmail.com>
2014-01-15 23:27:15 -0500

    reg name

---
+++ Assets/Scripts/EnterName.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterName : MonoBehaviour {
+
+	public float speed;
+
+	void Update () {
+		if(Input.GetButtonDown("Left") && transform.position.x > -97){
+			transform.position += new Vector3(-speed, 0);
+		}
+
+		if(Input.GetButtonDown("Right") && transform.position.x < 92){
+			transform.position += new Vector3(speed, 0);
+		}
+
+		if(Input.GetButtonDown("Up") && transform.position.y < 12){
+			transform.position += new Vector3(0, speed);
+		}
+
+		if(Input.GetButtonDown("Down") && transform.position.y > -43){
+			transform.position += new Vector3(0, -speed);
+		}
+
+	}
+}
--- Assets/Scripts/Link.cs
+++ Assets/Scripts/Link.cs
@@ -5,10 +5,12 @@ public class Link : MonoBehaviour {
 
 	public Sprite[] spr;
 	public float speed;
+	float initSpeed;
 
 	SpriteRenderer sprRend;
 
 	void Start () {
+		initSpeed = speed;
 		sprRend = renderer as SpriteRenderer;
 	}
 
@@ -24,24 +26,24 @@ public class Link : MonoBehaviour {
 		if(!Input.GetButton("Up") || !Input.GetButton("Down")){
 			if(vert == 1f){
 				sprRend.sprite = spr[2];
-				transform.Translate(new Vector2(0, speed));
+				transform.Translate(new Vector2(0, speed * Time.deltaTime));
 			}
 			
 			if(vert == -1f){
 				sprRend.sprite = spr[0];
-				transform.Translate(new Vector2(0, -speed));
+				transform.Translate(new Vector2(0, -speed * Time.deltaTime));
 			}
 		}
 
 		if(vert == 0 && (!Input.GetButton("Left") || !Input.GetButton("Right"))){
 			if(hor == -1f){
 				sprRend.sprite = spr[1];
-				transform.Translate(new Vector2(-speed, 0));
+				transform.Translate(new Vector2(-speed * Time.deltaTime, 0));
 			}
 			
 			if(hor == 1f){
 				sprRend.sprite = spr[3];
-				transform.Translate(new Vector2(speed, 0));
+				transform.Translate(new Vector2(speed * Time.deltaTime, 0));
 			}
 		}
 	}
@@ -51,7 +53,7 @@ public class Link : MonoBehaviour {
 			speed = 0;
 		}
 		else {
-			speed = 1;
+			speed = initSpeed;
 		}
 	}
 }
