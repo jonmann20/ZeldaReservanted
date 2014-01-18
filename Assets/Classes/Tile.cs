1107d055158c7058f6286943f026c112237ca063
ayarger <austinyarger@Austin-Yargers-MacBook-Pro.local>
2014-01-18 12:45:56 -0500

    fixed last commit-- added both Tile and Room files

---
+++ Assets/Classes/Tile.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile {
+	//local coords (coords in room)
+	int xcoord, ycoord;
+	string hexval;
+
+	//denote enemy / item placement on tile
+	string spawnval;
+
+	public Tile(int x, int y, string hex, string spawn)
+	{
+		xcoord = x;
+		ycoord = y;
+		hexval = hex;
+		spawnval = spawn;
+	}
+
+	//todo: replace these with get/set c# snippets?
+	public void alterTileHex(string hex)
+	{
+		hexval = hex;
+	}
+	public void alterTileSpawn(string spawn)
+	{
+		spawnval = spawn;
+	}
+}
