82d8d5054407271700460ae9166a78997553bfd7
ayarger <austinyarger@Austin-Yargers-MacBook-Pro.local>
2014-01-18 12:43:48 -0500

    Room and Tile Classes added, non-tested

---
+++ Assets/Classes/Room.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Room {
+	int xcoord, ycoord;
+	Tile[,] tiles = new Tile[16, 11];
+
+	//t is a 16x11 2d array of Tiles
+	public Room(int x, int y, Tile[,] t)
+	{
+		xcoord = x;
+		ycoord = y;
+		Array.Copy(t, 0, tiles, 0, 16);
+	}
+}
