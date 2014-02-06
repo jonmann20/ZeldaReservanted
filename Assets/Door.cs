42c9400310f990cdaa12b6fc3771f912d800b033
Jon Wiedmann <jonwiedmann@gmail.com>
2014-02-05 21:16:05 -0500

    dungeon tiles

---
+++ Assets/Door.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+	public static SpriteDir doorSide;
+
+	void Awake(){
+
+		doorSide = SpriteDir.LEFT;
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+
+		if(col.gameObject.tag == "Player"){
+			Dungeon.that.changeRoom(doorSide);
+		}
+	}
+}
