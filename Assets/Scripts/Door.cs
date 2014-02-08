567bb1b23f603b369c299020363f4fe0cbc6fc38
Jon Wiedmann <jonwiedmann@gmail.com>
2014-02-08 16:20:22 -0500

    boss --> tri --> main; room6 --> main

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class Door : MonoBehaviour {
 	public SpriteDir doorSide;
+	public bool isEntrance = false;		// NOTE: roomRBL is hardcoded with this info
 
 	void OnTriggerEnter2D(Collider2D col){
-		if(col.gameObject.tag == "Player"){
+		if(isEntrance){
+			Application.LoadLevel("main");
+		}
+		else if(col.gameObject.tag == "Player"){
 			Dungeon.that.changeRoom(doorSide);
 		}
 	}
---
+++ Assets/Scripts/Triforce.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Triforce : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col){
+		Application.LoadLevel("main");
+	}
+}
