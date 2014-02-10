1509b31555aaed4b03df6b68a9a169e376c9360b
ayarger <austinyarger@Austin-Yargers-MacBook-Pro.local>
2014-02-10 13:42:45 -0500

    SpikeEnemy implemented, Boss altered slightly

--- Assets/Scripts/Enemies/Spikes.cs
+++ Assets/Scripts/Enemies/Spikes.cs
@@ -4,21 +4,80 @@ using System.Collections;
 public class Spikes : Enemy {
 
 	Vector3 initialPosition;
-	const float chargeSpeed = 5;
-	const float reelSpeed = chargeSpeed * 0.5f;
+	const float MAX_DISTANCE = 5;
+	const float CHARGE_SPEED = 0.1f;
+	const float REEL_SPEED = CHARGE_SPEED * 0.25f;
 
-	//0 = idle, 1 = charging, 2 = reeling
+	//0 = idle, 1 = charging right, 2 = charging left, 3 = reeling
 	int state = 0;
 
 	// Use this for initialization
 	void Start () {
+		health = 100;
 		initialPosition = transform.position;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	public override void customUpdate()
+	{
+		//idle
+		if(state == 0)
+		{
+			float absYDiff = Mathf.Abs(Link.that.transform.position.y - transform.position.y);
+			float xDiff = Link.that.transform.position.x - transform.position.x;
+			if(absYDiff < 1)
+			{
+				if(xDiff > 0) //charge right
+					state = 1;
+				else if(xDiff < 0)  //charge left
+					state = 2;
+			}
+
+			return;
+		}
+		//charging right
+		else if(state == 1)
+		{
+			transform.Translate(new Vector3(CHARGE_SPEED, 0, 0));
+			float distanceFromInitialPos = Mathf.Abs(transform.position.x - initialPosition.x);
+			if(distanceFromInitialPos > MAX_DISTANCE)
+			{
+				state = 3;
+			}
+			return;
+		}
+		//charging left
+		else if(state == 2)
+		{
+			transform.Translate(new Vector3(-CHARGE_SPEED, 0, 0));
+			float distanceFromInitialPos = Mathf.Abs(transform.position.x - initialPosition.x);
+			if(distanceFromInitialPos > MAX_DISTANCE)
+			{
+				state = 3;
+			}
+			return;
+		}
+		//reeling
+		else if(state == 3)
+		{
+			if(initialPosition.x - transform.position.x > 0)
+			{
+				transform.Translate(new Vector3(REEL_SPEED, 0, 0));
+			}
+			else if(initialPosition.x - transform.position.x < 0)
+			{
+				transform.Translate(new Vector3(-REEL_SPEED, 0, 0));
+			}
+
+			float distanceFromInitialPos = Mathf.Abs(transform.position.x - initialPosition.x);
+			if(distanceFromInitialPos < 0.1f)
+			{
+				transform.position = initialPosition;
+				state = 0;
+			}
+			return;
+		}
 	}
+
 
 	public override void Movement(){
 
